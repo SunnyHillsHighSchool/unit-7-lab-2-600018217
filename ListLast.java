a902bbcb3e7d04af35f3ae716961f1f454a14068
Johnathan Mitri <600018217@fjuhsd.org>
2021-02-02 03:09:29 +0000

    Wrote code for Unit 7 Lab 2

--- ListLast.java
+++ ListLast.java
@@ -10,6 +10,20 @@ public class ListLast
 {
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
+    //save the last element in an int
+    int lastElement = ray.get(ray.size() - 1);
+    //for loop to loop through every element except the last one
+    for (int i = 0; i < ray.size() - 1; i++)
+    {
+    	//if the current element is equal to the last element
+      if (ray.get(i) == lastElement)
+      {
+        //return true
+        return true;
+      } 
+      //if not, keep searching
+    }				
+    //if it gets here, then the search never found a duplicate, so return false
+		return false;
 	}
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Johnathan Mitri
+//Date - 2-1-2021
 
 //add imports as needed
+import java.util.ArrayList;
+import java.util.List;
+import static java.lang.System.*;
+import java.util.Collections;
+import java.util.Arrays;
 
 public class Main
 {
  public static void main( String args[] )
  {
+    ListLast obj = new ListLast();
+    System.out.println(obj.go(Arrays.asList(-99,1,2,3,4,5,6,7,8,9,10,5)));
+   
  }
 }
